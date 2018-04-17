5911e7705e497cf41b3d49c5086609d0c70acb21
DEVILKXH <752527107@qq.com>
2018-04-17 18:22:07 +0800

    m

--- workspace/app.js
+++ workspace/app.js
@@ -2,20 +2,48 @@
 var app = getApp();
 App({
   onLaunch: function() {
-    var _this = this;
-    wx.login({
+    let _this = this;
+    wx.getUserInfo({
       success: function(res){
-        console.log(res);
+        _this.globalData.userInfo = res.userInfo;
+        _this.globalData.isLogin = true;
+        wx.login({
+          success: function (res) {
+            console.log(res);
+            if (res) {
+              wx.request({
+                url: 'https://api.weixin.qq.com/sns/jscode2session',
+                data: {
+                  appid: _this.globalData.APP_ID,
+                  secret: _this.globalData.APP_SECRET,
+                  js_code: res.code,
+                  grant_type: _this.globalData.GRANT_TYPE
+                },
+                success: function (response) {
+                  if (response.statusCode == 200) {
+                    _this.globalData.OPEN_ID = response.data.openid;
+                  }
+                }
+              })
+            }
+          }
+        });
+      },
+      fail: function(res){
+        //alert("授权失败");
+        _this.globalData.isLogin = false;
       }
-    })
+    });
   },
 
   globalData: {
     userInfo: null,
     location: "",
-    openId: '',
-    appId: 'wx9aacc28ca08872f0',
-    secret: '7bc128b26612a705deb0beee1804a8a5',
+    OPEN_ID: '',
+    isLogin: false,
+    APP_ID: 'wx9aacc28ca08872f0',
+    APP_SECRET: '7bc128b26612a705deb0beee1804a8a5',
+    GRANT_TYPE: 'authorization_code',
     uri: 'http://127.0.0.1:8080/demo-web/'
   }
 })
